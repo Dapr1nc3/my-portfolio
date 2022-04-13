2f5afa5292edfc63ce515df6988508ff29436b83
dapr1nc3 <ddaprincem84@gmail.com>
2022-04-13 12:48:32 -0400

    addede floating nav component using useState to have them corispond to id's

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -5,23 +5,46 @@ import { FaUser } from "react-icons/fa";
 import { BsFillJournalBookmarkFill } from "react-icons/bs";
 import { BsFillFileEarmarkFontFill } from "react-icons/bs";
 import { IoIosContact } from "react-icons/io";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <FaHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <FaUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <BsFillJournalBookmarkFill />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
         <BsFillFileEarmarkFontFill />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <IoIosContact />
       </a>
     </nav>
