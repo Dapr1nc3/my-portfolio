30e511f5b6d36e704ead714a07afd5d6861c37b2
dapr1nc3 <ddaprincem84@gmail.com>
2022-04-13 19:26:37 -0400

    made contact component and styled also responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import "./contact.css";
+import { MdEmail } from "react-icons/md";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_ofbv2vg",
+      "template_u69v30p",
+      form.current,
+      "Fth039GCYhwYPM5Y4"
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5 className="center__heading">Get In Touch</h5>
+      <h2 className="center__heading">Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>ddaprincem84@gmail.com</h5>
+            <a href="mailto:ddaprincem84@gmail.com" target="__blank">
+              Send a Email
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary btn__align">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./footer.css";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Devin
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+          <a href="#about">About</a>
+          <a href="#experience">Experience</a>
+          <a href="#services">Services</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com/daprinceee1">
+          <BsTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Devin Minnis. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
