ee5433a998b37a43389d0ac3683bd1057fa641bd
dapr1nc3 <ddaprincem84@gmail.com>
2022-04-12 23:19:17 -0400

    working on header adding img and styling the header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me-1.JPG";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Devin Minnis</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+
+        <div className="myself">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/devin-minnis-082a95217/"
+        target="_blank"
+      ></a>
+      <a href="https://github.com/Dapr1nc3" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
