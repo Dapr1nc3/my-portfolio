9d5d66e7619b1130a16999cb45904826bc7b0597
dapr1nc3 <ddaprincem84@gmail.com>
2022-04-13 11:59:26 -0400

    addeing navbar with react-icons giving wach section id to match

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./navbar.css";
+import { FaHome } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { BsFillJournalBookmarkFill } from "react-icons/bs";
+import { BsFillFileEarmarkFontFill } from "react-icons/bs";
+import { IoIosContact } from "react-icons/io";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <a href="#">
+        <FaHome />
+      </a>
+      <a href="#about">
+        <FaUser />
+      </a>
+      <a href="#experience">
+        <BsFillJournalBookmarkFill />
+      </a>
+      <a href="#portfolio">
+        <BsFillFileEarmarkFontFill />
+      </a>
+      <a href="#contact">
+        <IoIosContact />
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
