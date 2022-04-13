fac0be94ea04576cc3e1246f1c9af44e495f2717
dapr1nc3 <ddaprincem84@gmail.com>
2022-04-13 17:47:31 -0400

    done styling portfolio section but not responsive yet

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,8 +10,8 @@ import IMG6 from "../../assets/NGF-SS.png";
 const Portfolio = () => {
   return (
     <section id="portfolio">
-      <h5>My Projects</h5>
-      <h2>Portfolio</h2>
+      <h5 className="center__heading">My Projects</h5>
+      <h2 className="center__heading">Portfolio</h2>
 
       <div className="container portfolio__container">
         <article className="portfolio__item">
@@ -19,16 +19,108 @@ const Portfolio = () => {
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            GitHub
-          </a>
-          <a
-            href="https://github.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live App
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live App
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live App
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live App
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live App
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live App
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live App
+            </a>
+          </div>
         </article>
       </div>
     </section>
